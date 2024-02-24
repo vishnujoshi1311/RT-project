9c304f2cd14817ebc070c3e56457bc04253d1a74
Vishnu joshi <vishnuakj59@gmail.com>
2024-02-24 21:13:53 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import navbar from './components/navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-      <div className='Hero'>
-           <ul>
-            <li>HOme</li>
-            <li>Contact</li>
-            <li>Services</li>
-            <li>Help?</li>
-            <li>Cart</li>
-           </ul>        
-      </div>
-      <div><a href="">THE HEITAGE</a></div>
-      <div>dgsjdgeiwygdu</div>
-    </h1>
+    <>
+    <div>
+      huguj
+    </div>
+    
+    </>
     
   )
   
