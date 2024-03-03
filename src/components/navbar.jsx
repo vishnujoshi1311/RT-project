f14acacaff72005b8913777a2f69071324924a37
Vishnu joshi <vishnuakj59@gmail.com>
2024-03-03 17:19:34 +0530

    added hero section

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import {AiOutlineMenu,AiOutlineSearch,AiOutlineClose} from 'react-icons/ai'
-import {BsFillCartFill} from 'react-icons/bs'
+import React,  {useState} from 'react'
+import {AiOutlineMenu,AiOutlineSearch,AiOutlineClose,AiFillTag} from 'react-icons/ai'
+import {BsFillCartFill,BsFillSaveFill} from 'react-icons/bs'
+import {TbTruckDelivery} from 'react-icons/tb'
+import {MdFavorite,MdHelp} from 'react-icons/md'
+import {FaWallet,FaUserFriends} from 'react-icons/fa'
 
 const Navbar = () => {
+const [nav, setnav] = useState(false)  
   return (
     <>
     <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
     <div className='flex items-center'>
-    <div className='cursor-pointer'>
+    <div onClick={()=>  setnav(!nav)} className='cursor-pointer'>
       <AiOutlineMenu size={30} />
     </div>
     <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>Best <span className='font-bold'>Eats</span>
@@ -32,25 +36,27 @@ const Navbar = () => {
         </button>
 
         {/*mobile menu*/}
-         
-         <div  className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div>
+         {nav ?  <div  className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div>:''}
 
+        
          {/*sidedoorn menu*/}
          
-         <div className='fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300'>
-          <AiOutlineClose size={30} className='absolute right-4 top-4 cursor-pointer'/>
+         <div className={nav ? 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300' : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300'}>
+          <AiOutlineClose onClick={()=> setnav(!nav)} size={30} className='absolute right-4 top-4 cursor-pointer'/>
           <h2 className='text-2xl p-4'>Best <span className='font-bold'>Eats</span></h2>
-
+          <nav>
+            <ul className='flex flex-col p-4 text-gray-800'>
+              <li className='text-xl py-4 flex'><TbTruckDelivery size={25} className='mr-4' />Orders</li>
+              <li className='text-xl py-4 flex'><MdFavorite size={25} className='mr-4' />Favorites</li>
+              <li className='text-xl py-4 flex'><FaWallet size={25} className='mr-4' />Wallet</li>
+              <li className='text-xl py-4 flex'><MdHelp size={25} className='mr-4' />Help</li>
+              <li className='text-xl py-4 flex'><AiFillTag size={25} className='mr-4' />Promotions</li>
+              <li className='text-xl py-4 flex'><BsFillSaveFill size={25} className='mr-4' />Best Ones</li>
+              <li className='text-xl py-4 flex'><FaUserFriends size={25} className='mr-4' />Invite Friends</li>
+            </ul>
+          </nav>
           </div>
-
-
-     
-
-          
-
-
-
-    </div>
+         </div>
     </>
   )
 }
