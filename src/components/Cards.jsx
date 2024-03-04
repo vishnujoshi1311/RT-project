87634313c728e091e20e7d907f07ed68f46c9bf8
Vishnu joshi <vishnuakj59@gmail.com>
2024-03-04 22:23:35 +0530

    added cards section

---
+++ src/components/Cards.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div className='max-w-p[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
+        <div>
+            <div>
+                <p>Sun's Out,BOGO's Out </p>
+                <p>Through 8/26</p>
+                <button>Order Now</button>
+            </div>
+            <img src="https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Cards
