bb43b2885492eb0674fca2a6bd0b7ebb1669906e
Vishnu joshi <vishnuakj59@gmail.com>
2024-03-09 13:46:49 +0530

    added new file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Cards from './components/Cards'
 import Food from './components/Food'
+import Category from './components/Category'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero/>
       <Cards/>
       <Food/>
+      <Category/>
     </div>
   )
 }
